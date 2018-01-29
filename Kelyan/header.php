f3413d49c63255bc3c15d7811d50464ced35d8e7
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-01-29 15:39:10 -0500

    creation de la navbar

---
+++ Kelyan/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="navbar">
+        <a>Acceuil</a>
+        <a>Anime</a>
+        <a>Communauté</a>
+        <a>Boutique</a>
+        <a>Contact</a>
+        <input type="text" placeholder="Search..">
+    </div>
+</header>
