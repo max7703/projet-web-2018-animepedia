b01c97fa4bd5d4061d285d72826aad16360cf650
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-01-31 09:28:17 -0500

    ajout du footer

---
+++ Kelyan/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div>
+        <div>
+            <a href="faq">F.A.Q</a>
+            <a href="about">A propos</a>
+        </div>
+        <div>
+            Copyright © 2018 Animepedia.fr
+        </div>
+    </div>
+</footer>
