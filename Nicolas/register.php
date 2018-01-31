cd56b32e7da4c9b9b94263d491f1b7f1ab123812
Nicolas Damonneville <SkyzrisPlayer2906@gmail.com>
2018-01-31 08:41:12 -0500

    Debut du code d'inscription

--- Nicolas/register.php
+++ Nicolas/register.php
@@ -5,14 +5,28 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
-	<title></title>
+	<title>Register</title>
 	
-	<link rel="stylesheet" href="assets/styles/main.css">
+	<div class="form">
+	
+		<ul class="tab-group">
+			<li class="tab active"><a href="#signup">Sign Up</a></li>
+		</ul>
+	  
+		<div class="tab-content">
+			<div id="signup"> 
+			</div>
+		</div>
+		
+		
+	</div>
+	
+	<!-- <link rel="stylesheet" href="assets/styles/main.css"> -->
 	
 </head>
 <body>
 
-	<script src="assets/scripts/main.js"></script>
+	<!-- <script src="assets/scripts/main.js"></script> -->
 
 </body>
 </html>
