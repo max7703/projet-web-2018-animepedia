99de7c22400b1e9d5540c44a35b0ca4da46644ab
max7703 <max7703@live.fr>
2018-01-31 19:49:09 -0500

    ajout d'un carousel
    #6

--- Maxime/index.php
+++ Maxime/index.php
@@ -56,6 +56,50 @@
 	  <h1>My First Bootstrap Page</h1>
 	  <p>This is some text.</p>
     </div>
+  
+	<div id="demo" class="carousel slide" data-ride="carousel">
+
+	  <!-- Indicators -->
+	  <ul class="carousel-indicators">
+		<li data-target="#demo" data-slide-to="0" class="active"></li>
+		<li data-target="#demo" data-slide-to="1"></li>
+		<li data-target="#demo" data-slide-to="2"></li>
+	  </ul>
+
+	  <!-- The slideshow -->
+	  <div class="carousel-inner">
+		<div class="carousel-item active">
+		  <img src="https://vignette.wikia.nocookie.net/creepypasta/images/e/e2/Anime-Girl-With-Silver-Hair-And-Purple-Eyes-HD-Wallpaper.jpg/revision/latest?cb=20140120061808&format=original">
+		  <div class="carousel-caption">
+			<h3>Cute</h3>
+			<p>WTF!!</p>
+		  </div>
+		</div>
+		<div class="carousel-item">
+		  <img src="http://media.comicbook.com/2017/10/1536737-japanese-keyart-officialvideoimage-21cf3b84-5780-e611-80-1027211.jpg">
+		  <div class="carousel-caption">
+			<h3>Overlord</h3>
+			<p>WTF!!</p>
+		  </div>
+		</div>
+		<div class="carousel-item">
+		  <img src="https://myanimelist.cdn-dena.com/s/common/uploaded_files/1474091647-7123f5fef60976326133d0aa49d17e04.jpeg">
+		  <div class="carousel-caption">
+			<h3>Re Zero</h3>
+			<p>WTF!!</p>
+		  </div>
+		</div>
+	  </div>
+
+	  <!-- Left and right controls -->
+	  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+		<span class="carousel-control-prev-icon"></span>
+	  </a>
+	  <a class="carousel-control-next" href="#demo" data-slide="next">
+		<span class="carousel-control-next-icon"></span>
+	  </a>
+
+	</div>
 
 </body>
 </html> 
