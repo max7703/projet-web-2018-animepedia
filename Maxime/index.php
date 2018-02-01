c465f68f21a287556b76e72a5880fbbc915ebae3
max7703 <max7703@live.fr>
2018-01-31 19:48:34 -0500

    Ajout d'une navbar
    #6

--- Maxime/index.php
+++ Maxime/index.php
@@ -1,20 +1,61 @@
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <title>Animepedia</title>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <header>
-        <h1></h1>
-        <nav></nav>
-    </header>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+		  <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="false">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
 
-    <section id="contenu">
-        <header><h2></h2></header>
+		  <div class="navbar-collapse collapse" id="navb" style="">
+			<ul class="navbar-nav mr-auto">
+			  <li class="nav-item">
+				<a class="nav-link" href="javascript:void(0)">Animes</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="javascript:void(0)">Communautés</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="javascript:void(0)">Boutique</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="javascript:void(0)">Contact</a>
+			  </li>
+			  <!-- Dropdown -->
+			  <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+				Dropdown link
+			    </a>
+			    <div class="dropdown-menu">
+				  <a class="dropdown-item" href="#">Link 1</a>
+				  <a class="dropdown-item" href="#">Link 2</a>
+				  <a class="dropdown-item" href="#">Link 3</a>
+			    </div>
+			</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+			  <input class="form-control mr-sm-2" placeholder="Search" type="text">
+			  <button class="btn btn-success my-2 my-sm-0" type="button">Search</button>
+			</form>
+		  </div>
+		</nav>
+	</header>
 
-    </section>
+	<div class="container">
+	  <h1>My First Bootstrap Page</h1>
+	  <p>This is some text.</p>
+    </div>
 
-    <footer><span id="signature"></span></footer>
 </body>
-</html>
+</html> 
