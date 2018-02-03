a1d26fd0f1b78bd1ef5a17dd9b1f0ccda43950b0
johnathan-sadin <johnathan60@hotmail.fr>
2018-02-03 18:49:02 -0500

    Passage a la v4
    references #11

--- Johnathan/anime.php
+++ Johnathan/anime.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Anime</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 
     <div class="container">
         <ul class="pagination">
             <li class="page-item"><a class="page-link" href="#">Tous</a></li>
-            <li><a href="#">A</a></li>
-            <li><a href="#">B</a></li>
-            <li><a href="#">C</a></li>
-            <li><a href="#">D</a></li>
-            <li><a href="#">E</a></li>
-            <li><a href="#">F</a></li>
-            <li><a href="#">G</a></li>
-            <li><a href="#">H</a></li>
-            <li><a href="#">I</a></li>
-            <li><a href="#">J</a></li>
-            <li><a href="#">K</a></li>
-            <li><a href="#">L</a></li>
-            <li><a href="#">M</a></li>
-            <li><a href="#">N</a></li>
-            <li><a href="#">O</a></li>
-            <li><a href="#">P</a></li>
-            <li><a href="#">Q</a></li>
-            <li><a href="#">R</a></li>
-            <li><a href="#">S</a></li>
-            <li><a href="#">T</a></li>
-            <li><a href="#">U</a></li>
-            <li><a href="#">V</a></li>
-            <li><a href="#">W</a></li>
-            <li><a href="#">X</a></li>
-            <li><a href="#">Y</a></li>
-            <li><a href="#">Z</a></li>
+            <li class="page-item"><a class="page-link" href="#">A</a></li>
+            <li class="page-item"><a class="page-link" href="#">B</a></li>
+            <li class="page-item"><a class="page-link" href="#">C</a></li>
+            <li class="page-item"><a class="page-link" href="#">D</a></li>
+            <li class="page-item"><a class="page-link" href="#">E</a></li>
+            <li class="page-item"><a class="page-link" href="#">F</a></li>
+            <li class="page-item"><a class="page-link" href="#">G</a></li>
+            <li class="page-item"><a class="page-link" href="#">H</a></li>
+            <li class="page-item"><a class="page-link" href="#">I</a></li>
+            <li class="page-item"><a class="page-link" href="#">J</a></li>
+            <li class="page-item"><a class="page-link" href="#">K</a></li>
+            <li class="page-item"><a class="page-link" href="#">L</a></li>
+            <li class="page-item"><a class="page-link" href="#">M</a></li>
+            <li class="page-item"><a class="page-link" href="#">N</a></li>
+            <li class="page-item"><a class="page-link" href="#">O</a></li>
+            <li class="page-item"><a class="page-link" href="#">P</a></li>
+            <li class="page-item"><a class="page-link" href="#">Q</a></li>
+            <li class="page-item"><a class="page-link" href="#">R</a></li>
+            <li class="page-item"><a class="page-link" href="#">S</a></li>
+            <li class="page-item"><a class="page-link" href="#">T</a></li>
+            <li class="page-item"><a class="page-link" href="#">U</a></li>
+            <li class="page-item"><a class="page-link" href="#">V</a></li>
+            <li class="page-item"><a class="page-link" href="#">W</a></li>
+            <li class="page-item"><a class="page-link" href="#">X</a></li>
+            <li class="page-item"><a class="page-link" href="#">Y</a></li>
+            <li class="page-item"><a class="page-link" href="#">Z</a></li>
         </ul>
     </div>
 
     <!-- Left-aligned media object -->
-    <div class= "col-md-offset-2 media">
+    <div class= "col-md-8 offset-2 media">
         <div class="media-left">
             <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/1/1a/Logo_One_piece.svg/250px-Logo_One_piece.svg.png" class="media-object" style="width:200px">
         </div>
@@ -53,7 +53,7 @@
             <p><a href="#" class="btn btn-primary" role="button">Voir »</a></p>
         </div>
     </div>
-    <div class= "col-md-offset-2 media">
+    <div class= "col-md-8 offset-2 media">
         <div class="media-left">
             <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/Naruto_logo.svg" class="media-object" style="width:200px">
         </div>
@@ -64,7 +64,7 @@
             <p><a href="#" class="btn btn-primary" role="button">Voir »</a></p>
         </div>
     </div>
-    <div class= "col-md-offset-2 media">
+    <div class= "col-md-8 offset-2 media">
         <div class="media-left">
             <img src="https://upload.wikimedia.org/wikipedia/fr/0/0e/My_Hero_Academia_logo_fr.png" class="media-object" style="width:200px">
         </div>
