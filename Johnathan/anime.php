af06ce0a8d5357054a7de5516333a36c763f79c4
johnathan-sadin <johnathan60@hotmail.fr>
2018-02-03 18:24:50 -0500

    Page anime
    references #11

--- Johnathan/anime.php
+++ Johnathan/anime.php
@@ -1,54 +1,78 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<div class="container">
-    <ul class="pagination">
-        <li class="page-item"><a class="page-link" href="#">Tous</a></li>
-        <li><a href="#">A</a></li>
-        <li><a href="#">B</a></li>
-        <li><a href="#">C</a></li>
-        <li><a href="#">D</a></li>
-        <li><a href="#">E</a></li>
-        <li><a href="#">F</a></li>
-        <li><a href="#">G</a></li>
-        <li><a href="#">H</a></li>
-        <li><a href="#">I</a></li>
-        <li><a href="#">J</a></li>
-        <li><a href="#">K</a></li>
-        <li><a href="#">L</a></li>
-        <li><a href="#">M</a></li>
-        <li><a href="#">N</a></li>
-        <li><a href="#">O</a></li>
-        <li><a href="#">P</a></li>
-        <li><a href="#">Q</a></li>
-        <li><a href="#">R</a></li>
-        <li><a href="#">S</a></li>
-        <li><a href="#">T</a></li>
-        <li><a href="#">U</a></li>
-        <li><a href="#">V</a></li>
-        <li><a href="#">W</a></li>
-        <li><a href="#">X</a></li>
-        <li><a href="#">Y</a></li>
-        <li><a href="#">Z</a></li>
-    </ul>
-</div>
+    <div class="container">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Tous</a></li>
+            <li><a href="#">A</a></li>
+            <li><a href="#">B</a></li>
+            <li><a href="#">C</a></li>
+            <li><a href="#">D</a></li>
+            <li><a href="#">E</a></li>
+            <li><a href="#">F</a></li>
+            <li><a href="#">G</a></li>
+            <li><a href="#">H</a></li>
+            <li><a href="#">I</a></li>
+            <li><a href="#">J</a></li>
+            <li><a href="#">K</a></li>
+            <li><a href="#">L</a></li>
+            <li><a href="#">M</a></li>
+            <li><a href="#">N</a></li>
+            <li><a href="#">O</a></li>
+            <li><a href="#">P</a></li>
+            <li><a href="#">Q</a></li>
+            <li><a href="#">R</a></li>
+            <li><a href="#">S</a></li>
+            <li><a href="#">T</a></li>
+            <li><a href="#">U</a></li>
+            <li><a href="#">V</a></li>
+            <li><a href="#">W</a></li>
+            <li><a href="#">X</a></li>
+            <li><a href="#">Y</a></li>
+            <li><a href="#">Z</a></li>
+        </ul>
+    </div>
 
     <!-- Left-aligned media object -->
-    <div class="media">
+    <div class= "col-md-offset-2 media">
         <div class="media-left">
-            <img src="https://fr.wikipedia.org/wiki/One_Piece#/media/File:Logo_One_piece.svg" class="media-object" style="width:128px">
+            <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/1/1a/Logo_One_piece.svg/250px-Logo_One_piece.svg.png" class="media-object" style="width:200px">
         </div>
         <div class="media-body">
-            <h4 class="media-heading">Left-aligned</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h4 class="media-heading">One Piece</h4>
+            <p>Ceci est du texte</p>
+            <br/>
+            <p><a href="#" class="btn btn-primary" role="button">Voir »</a></p>
+        </div>
+    </div>
+    <div class= "col-md-offset-2 media">
+        <div class="media-left">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c9/Naruto_logo.svg" class="media-object" style="width:200px">
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading">Naruto</h4>
+            <p>Ceci est du texte</p>
+            <br/>
+            <p><a href="#" class="btn btn-primary" role="button">Voir »</a></p>
+        </div>
+    </div>
+    <div class= "col-md-offset-2 media">
+        <div class="media-left">
+            <img src="https://upload.wikimedia.org/wikipedia/fr/0/0e/My_Hero_Academia_logo_fr.png" class="media-object" style="width:200px">
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading">My Hero Academia</h4>
+            <p>Ceci est du texte</p>
+            <br/>
+            <p><a href="#" class="btn btn-primary" role="button">Voir »</a></p>
         </div>
     </div>
 
