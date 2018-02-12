4be3308ba52b15d6f4b5d2b80fc1301bb78c01aa
Nicolas Damonneville <SkyzrisPlayer2906@gmail.com>
2018-02-12 14:44:05 -0500

    Fonction Ajouter Un Utilisateur
    #10

--- Nicolas/Dao/UtilisateurDAO.php
+++ Nicolas/Dao/UtilisateurDAO.php
@@ -13,7 +13,7 @@ class UtilisateurDAO
         foreach($req->fetchAll() as $utilisateur) 
 		{
             $list[] = new Utilisateur($utilisateur['id_utilisateur'], 
-			$utilisateur['nom_utilisateur'], 
+			$utilisateur['pseudo_utilisateur'], 
 			$utilisateur['mdp_utilisateur'], 
 			$utilisateur['email_utilisateur'], 
 			$utilisateur['id_privilege'], 
@@ -29,13 +29,45 @@ class UtilisateurDAO
 		$db = Db::getInstance();
         $id = intval($id);
 		
-        $req = $db->prepare('SELECT * FROM utilisateur WHERE id = :id');
+        $req = $db->prepare('SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur');
 		
-        $req->execute(array('id' => $id));
+        $req->execute(array('id_utilisateur' => $id));
 		
         $utilisateur = $req->fetch();
 
         return new Utilisateur($utilisateur['id_utilisateur'], 
-		$utilisateur['nom_utilisateur']);	
+		$utilisateur['pseudo_utilisateur'],
+		$utilisateur['mdp_utilisateur'], 
+		$utilisateur['email_utilisateur'], 
+		$utilisateur['id_privilege'], 
+		$utilisateur['image_utilisateur'], 
+		$utilisateur['description_utilisateur']);
+    }
+	
+	public function AjouterUnUtilisateur(Utilisateur $utilisateur)
+	{
+        $db = Db::getInstance();
+
+        $req = $db->prepare('INSERT INTO utilisateur(pseudo_utilisateur,
+		mdp_utilisateur, 
+		email_utilisateur, 
+        id_privilege, 
+		image_utilisateur,
+		description_utilisateur) 
+		
+		VALUES(:pseudo_utilisateur,  
+		:mdp_utilisateur, 
+		:email_utilisateur, 
+        :id_privilege, 
+		:image_utilisateur,
+		:description_utilisateur');
+
+        $req->execute(array('pseudo_utilisateur' => $utilisateur->getNom(), 
+		'mdp_utilisateur' => $utilisateur->getMdp(),
+		'email_utilisateur'=> $utilisateur->getEmail(),
+		'id_privilege'=> $utilisateur->getId_Privilege(),
+		'image_utilisateur'=> $utilisateur->getImage(),
+		'description_utilisateur'=> $utilisateur->getDescription()));
+
     }
 	
