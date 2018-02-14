9d9a916fbc23700212f8e482809580fd02940076
Nicolas Damonneville <SkyzrisPlayer2906@gmail.com>
2018-02-14 10:25:24 -0500

    Export de la table Privilege
    #23

---
+++ bdd/exports/privilege.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : animepedao77.mysql.db
+-- Généré le :  mer. 14 fév. 2018 à 16:19
+-- Version du serveur :  5.6.34-log
+-- Version de PHP :  7.0.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `animepedao77`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `privilege`
+--
+
+CREATE TABLE `privilege` (
+  `id_privilege` int(11) NOT NULL,
+  `nom_privilege` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `privilege`
+--
+
+INSERT INTO `privilege` (`id_privilege`, `nom_privilege`) VALUES
+(1, 'Administrateur'),
+(2, 'Utilisateur'),
+(3, 'Abonn');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `privilege`
+--
+ALTER TABLE `privilege`
+  ADD PRIMARY KEY (`id_privilege`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `privilege`
+--
+ALTER TABLE `privilege`
+  MODIFY `id_privilege` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
