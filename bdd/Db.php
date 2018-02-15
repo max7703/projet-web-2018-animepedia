9b73b9a1e6e346af52f1d65d655c46bc4ca1e24c
max7703 <max7703@live.fr>
2018-02-14 22:32:07 -0500

    correction accent utf8
    #23

--- bdd/Db.php
+++ bdd/Db.php
@@ -16,7 +16,7 @@ class Db {
     public static function getInstance() {
         if (!isset(self::$instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$instance = new PDO('mysql:host=animepedao77.mysql.db;dbname=animepedao77', 'animepedao77', 'Ccleaner77', $pdo_options);
+            self::$instance = new PDO('mysql:host=animepedao77.mysql.db;dbname=animepedao77;charset=utf8', 'animepedao77', 'Ccleaner77', $pdo_options);
             if(self::$instance == NULL)
                 echo('Erreur de connexion');
         }
