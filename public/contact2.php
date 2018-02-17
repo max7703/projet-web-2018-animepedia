f7e34f6db80d3bce047f3cc0661848d6cc324ccf
johnathan-sadin <johnathan60@hotmail.fr>
2018-02-17 18:14:12 -0500

    Ajout page test contact
    Ajout d'une nouvelle page pour faire fonctionner le php_filter
    #26

---
+++ public/contact2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <form action="contact2_verif.php" method="post">
+        <h2>Formulaire test</h2>
+        Nom:
+        <input class="input" name="nom" type="text" value="">
+        Prenom:
+        <input class="input" name="prenom" type="text" value="">
+        E-mail:
+        <input class="input" name="email" type="text" value="">
+        <input class="submit" name="submit" type="submit" value="Submit">
+    </form>
+</body>
+</html>
