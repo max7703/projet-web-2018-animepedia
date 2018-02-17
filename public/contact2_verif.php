5dc3dc87b110e2c079c9ea0453cf61ccbb475c76
johnathan-sadin <johnathan60@hotmail.fr>
2018-02-17 18:27:11 -0500

    Test php_filter pour l'adresse mail
    #26

--- public/contact2_verif.php
+++ public/contact2_verif.php
@@ -2,11 +2,12 @@
 // Initialisation des variables d'erreur.
 $nonErreur ="";
 $prenomErreur ="";
+$emailErreur ="";
 
 // Initialisation du code lorsque l'on appuie sur le bouton envoyer
 if(isset($_POST["submit"])) {
     /*--------------------------------------------------------------
-    Récuperer la valeur du nom depuis l'URL et le nettoyer
+    Récuperer la valeur du nom et le nettoyer
     --------------------------------------------------------------*/
     if ($_POST["nom"] != "") {
         // Nettoyer la valeur nom de type string
@@ -20,7 +21,7 @@ if(isset($_POST["submit"])) {
     }
 
     /*--------------------------------------------------------------
-    Récuperer la valeur du prénom et le nettoyer
+    Récuperer la valeur du prénom  et le nettoyer
     --------------------------------------------------------------*/
     if ($_POST["prenom"] != "") {
         // Nettoyer la valeur prénom de type string
@@ -33,6 +34,21 @@ if(isset($_POST["submit"])) {
         $prenomErreur = "<span>S'il vous plaît, entrer votre prénom.</span>";
     }
 
+    /*------------------------------------------------------------------
+    Récuperer la valeur de l'email, le nettoyer et le valider
+    --------------------------------------------------------------------*/
+    if ($_POST["email"] != "") {
+        // Nettoyage des caractères non autorisés
+        $_POST["email"] = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+        // Validation de l'email
+        $_POST["email"] = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+        $emailErreur = "<span class=\"valid\">\"" . $_POST["email"] . "\" </span>est nettoyé et valide";
+        if ($_POST["email"] == "") {
+            $emailErreur = "<span>S'il vous plaît, entrer un email valide.</span>";
+        }
+    } else {
+        $emailErreur = "<span>S'il vous plaît, entrer votre email.</span>";
+    }
 }
 ?>
 
@@ -41,3 +57,5 @@ if(isset($_POST["submit"])) {
 <p><?php echo $nomErreur;?></p>
 <p><?php echo $_POST["prenom"];?></p>
 <p><?php echo $prenomErreur;?></p>
+<p><?php echo $_POST["email"];?></p>
+<p><?php echo $emailErreur;?></p>
