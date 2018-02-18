8eb7d6abd5f62a26cc8e982c6b5daa9e52560907
johnathan-sadin <johnathan60@hotmail.fr>
2018-02-18 10:56:12 -0500

    Mise en place de la nouvelle page contact a partir de l'ancienne
    #26

--- vue/VueContact.php
+++ vue/VueContact.php
@@ -12,53 +12,53 @@ $header = new VueHeader();
 $header->printHeader("Contact");
 
 echo '
-<div class="controls pt-3 offset-2">
+<form action="modele/Contact_Verif.php" method="post" role="form">
 
-	<div class="row">
-		<div class="col-md-5">
-			<div class="form-group">
-				<label for="form_name">Prénom *</label>
-				<input id="form_name" type="text" name="name" class="form-control" placeholder="Entrez votre prénom *" required="required" data-error="Le prénom est requis.">
-				<div class="help-block with-errors"></div>
-			</div>
-		</div>
-		<div class="col-md-5">
-			<div class="form-group">
-				<label for="form_lastname">Nom *</label>
-				<input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Entrez votre nom *" required="required" data-error="Le nom est requis.">
-				<div class="help-block with-errors"></div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-5">
-			<div class="form-group">
-				<label for="form_email">Email *</label>
-				<input id="form_email" type="email" name="email" class="form-control" placeholder="Entrez votre email *" required="required" data-error="Un email valide est requis.">
-				<div class="help-block with-errors"></div>
-			</div>
-		</div>
-		<div class="col-md-5">
-			<div class="form-group">
-				<label for="form_phone">Téléphone</label>
-				<input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Entrez votre numéro de téléphone">
-				<div class="help-block with-errors"></div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-10">
-			<div class="form-group">
-				<label for="form_message">Message *</label>
-				<textarea id="form_message" name="message" class="form-control" placeholder="Tapez votre message *" rows="4" required="required" data-error="S\'il vous plaît, laissez un meaage."></textarea>
-				<div class="help-block with-errors"></div>
-			</div>
-		</div>
-		<div class="col-md-12">
-			<input type="submit" class="btn btn-success btn-send" value="Envoyer">
-		</div>
-	</div>
-</div>
+    <div class="messages pt-4"></div>
+
+    <div class="controls offset-2">
+
+        <div class="row">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label>Prénom *</label>
+                    <input type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom *">
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label>Nom *</label>
+                    <input type="text" name="nom" class="form-control" placeholder="Entrez votre nom *">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label>Email *</label>
+                    <input type="email" name="email" class="form-control" placeholder="Entrez votre email *">
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group">
+                    <label>Téléphone</label>
+                    <input type="tel" name="telephone" class="form-control" placeholder="Entrez votre numéro de téléphone">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10">
+                <div class="form-group">
+                    <label>Message *</label>
+                    <textarea name="message" class="form-control" placeholder="Tapez votre message *" rows="4"></textarea>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <input type="submit" name="submit" class="btn btn-success btn-send" value="Envoyer">
+            </div>
+        </div>
+    </div>
+</form>
 
 ';
 
