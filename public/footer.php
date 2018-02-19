5c73b37c3e5f171bc32b996aac83282c53967ef4
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-02-18 22:31:39 -0500

    refactor footer
    #29

--- public/footer.php
+++ public/footer.php
@@ -1,44 +1,45 @@
-<link rel="stylesheet" href="footer_.css">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max77
+ * Date: 12/02/2018
+ * Time: 13:36
+ */
+?>
+<link rel="stylesheet" href=<?php echo CSS_PIEDDEPAGE?>>
 <footer class="footer">
     <div class="container">
         <div class="row">
         <div class="col-sm-3">
-            <h4 class="title">A propos</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit, libero a molestie consectetur, sapien elit lacinia mi.</p>
-            <ul class="social-icon">
-                <a href="#" class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-                <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
-                <a href="#" class="social"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
-            </ul>
+            <h4 class="title"><?php echo _("A propos")?></h4>
+            <p> <?php echo _("Nous sommes une équipe de 4 étudiants en DEC Technique de l'Informatique - Informatique de Gestion. Nous avons eu l'idée de créer un site qui redirige l'utilisateur
+                vers un lien fiable où il pourra regarder son animé, le tout sans pub.")?></p>
         </div>
         <div class="col-sm-3">
-            <h4 class="title">Navigation</h4>
+            <h4 class="title"><?php echo _("Navigation")?></h4>
             <span class="acount-icon">          
-            <a href="#"><i class="fa fa-heart" aria-hidden="true"></i> List</a>
-            <a href="#"><i class="fa fa-users" aria-hidden="true"></i>Group</a>
-            <a href="#"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
-            <a href="#"><i class="fa fa-globe" aria-hidden="true"></i> Langue</a>           
+            <a href="#"><i class="fa fa-heart" aria-hidden="true"></i> <?php echo _("List")?></a>
+            <a href="#"><i class="fa fa-users" aria-hidden="true"></i><?php echo _("Group")?></a>
+            <a href="#"><i class="fa fa-user" aria-hidden="true"></i> <?php echo _("Profile")?></a>
+            <a href="#"><i class="fa fa-globe" aria-hidden="true"></i> <?php echo _("Langue")?></a>
             </span>
         </div>
         <div class="col-sm-3">
-            <h4 class="title">Categories</h4>
+            <h4 class="title"><?php echo _("Categories")?></h4>
             <div class="category">
-                <a href="#">Ecchi</a>
-                <a href="#">Seinen</a>
-                <a href="#">Shōjo</a>
-                <a href="#">Shōnen</a>
-                <a href="#">Yaoi</a>
-                <a href="#">Yandere</a>
-                <a href="#">Yuri</a>
-                <a href="#">Magical girl</a>      
+                <a href="#"><?php echo _("Ecchi")?></a>
+                <a href="#"><?php echo _("Seinen")?></a>
+                <a href="#"><?php echo _("Shōjo")?></a>
+                <a href="#"><?php echo _("Shōnen")?></a>
+                <a href="#"><?php echo _("Yaoi")?></a>
+                <a href="#"><?php echo _("Yandere")?></a>
+                <a href="#"><?php echo _("Yuri")?></a>
+                <a href="#"><?php echo _("Magical girl")?></a>
             </div>
         </div>
         <div class="col-sm-3">
-            <h4 class="title">Moyens de paiement</h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <h4 class="title"><?php echo _("Moyens de paiement")?></h4>
+            <p><?php echo _("Divers moyens de paiement sont disponible sur notre site")?></p>
             <ul class="payment">
                 <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
                 <li><a href="#"><i class="fa fa-credit-card" aria-hidden="true"></i></a></li>            
@@ -48,7 +49,7 @@
             </div>
         </div>
         <hr>
-        <div class="pl-1"><a href="http://dev.animepedia.fr/" style="color: #fff;">Copyright © Animepedia 2018</a></div>
+        <div class="pl-1"><a href="http://dev.animepedia.fr/" style="color: #fff;"><?php echo _("Copyright © Animepedia 2018")?></a></div>
     </div>	
 </footer>
 </body>
