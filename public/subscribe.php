f641bb76cb37c33641811e86dc614d6e83042f7c
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-02-18 22:33:27 -0500

    refactor page subscribe
    #29

--- public/subscribe.php
+++ public/subscribe.php
@@ -1,12 +1,27 @@
-<?php include 'header.php' ?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max77
+ * Date: 12/02/2018
+ * Time: 18:50
+ */
 
-<link rel="stylesheet" href="subscribe_.css">
+define("NOMDEPAGE", "Abonnement");
+include_once $_SERVER['DOCUMENT_ROOT'] . '/configuration.php';
+require ENTETE;
 
+if( isset($_POST['fromPerson']) )
+{
+    $fromPerson = '+from%3A'.$_POST['fromPerson'];
+    echo $fromPerson;
+}?>
+
+<link rel="stylesheet" href=<?php echo CSS_ABONNEMENT?>>
 <section class="section-padding">
    <div class="container">
 	   <div class="section-title">
-		   <h2>Pricing <span>Plans</span></h2>
-		   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid in ullam labore optio temporibus ex recusandae minima et ipsa aliquam!</p>
+		   <h2>Packs</h2>
+		   <p class="pt-2">Abonnez-vous pour nous soutenir et gagner quelques fonctionnalités en plus ^^</p>
 	   </div>
 	   <div class="single-pricing-box">
 		   <h3>Gratuit</h3>
@@ -15,15 +30,12 @@
 		   </div>
 		   <div class="pricing-list">
 			   <ul>
-			   <li>1GB web Space</li>
-			   <li>5+ Mysql Database</li>
-			   <li class="off">Managed CMS</li>
-			   <li>Unlimited Bandwidth</li>
-			   <li>Other features</li>
+			   <li class="off">Navigation sans pub</li>
+			   <li class="off">Rang Discord</li>
+			   <li class="off">Section Premium</li>
 		   </ul>
 		   </div>
 		   <h3 class="pricing-count">$0/mo</h3>
-		   <a href="" class="bordered-btn">Order Now</a>
 	   </div>
 	   <div class="single-pricing-box">
 		   <h3>Premium</h3>
@@ -32,16 +44,15 @@
 		   </div>
 		   <div class="pricing-list">
 			   <ul>
-			   <li>2GB web Space</li>
-			   <li>10+ Mysql Database</li>
-			   <li class="off">Managed CMS</li>
-			   <li>Unlimited Bandwidth</li>
-			   <li>Other features</li>
+			   <li>Navigation sans pub</li>
+			   <li>Rang Discord</li>
+			   <li>Section Premium</li>
 		   </ul>
 		   </div>
 		   <h3 class="pricing-count">$5/mo</h3>
-		   <a href="" class="bordered-btn">Order Now</a>
+		   <a href="wizard" class="bordered-btn">Commander</a>
 	   </div>
    </div>
 </section>
-<?php include 'footer.php' ?>
+
+<?php include PIEDDEPAGE;?>
