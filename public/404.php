82f9d13be96825d67305a08ac626ce2da38b85a6
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-02-18 22:30:31 -0500

    refactor page 404
    #29 #27

--- public/404.php
+++ public/404.php
@@ -1,22 +1,34 @@
-<?php include 'header.php' ?>
-<link rel="stylesheet" href="404_.css">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max77
+ * Date: 12/02/2018
+ * Time: 21:58
+ */
+
+define("NOMDEPAGE", "404");
+include_once $_SERVER['DOCUMENT_ROOT'] . '/configuration.php';
+require ENTETE;
+?>
+
+<link rel="stylesheet" href= <?php echo CSS . "/404_.css" ?>>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
             <div class="error-template">
                 <h1>
-                    Oops!</h1>
+                    <?php echo _("Oups!")?></h1>
                 <h2>
-                    404 Not Found</h2>
+                    <?php echo _("404 Not Found")?></h2>
                 <div class="error-details">
-                    Sorry, an error has occured, Requested page not found!
+                    <?php echo _("Sorry, an error has occured, Requested page not found!")?>
                 </div>
                 <div class="error-actions">
                     <a href="index" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
-                        Back to Home </a><a href="#" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
+                        <?php echo _("Back to Home")?> </a><a href="#" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> <?php echo _("Contact Support")?> </a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<?php include 'footer.php' ?>
+<?php include PIEDDEPAGE;?>
