ed752fcd7327255e35f76403fdf35b3be28bcaab
Kelyan CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2018-03-07 08:35:15 -0500

    test fix bug login
    #24

--- public/login.php
+++ public/login.php
@@ -7,10 +7,13 @@
  */
 define("NOMDEPAGE", "Connexion");
 include_once $_SERVER['DOCUMENT_ROOT'] . '/configuration.php';
-require ENTETE;
 
 if( isset($_SESSION['logged_in']))
-    header( "url: https://www.dev.animepedia.fr/home" );?>
+    header( "url: https://www.dev.animepedia.fr/home" );
+
+require ENTETE;?>
+
+
 
 <link rel="stylesheet" href=<?php echo CSS_CONNEXION?>>
 <div class="container">
